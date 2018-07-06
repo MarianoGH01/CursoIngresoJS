94631a6e8db2f73d64cb5ffcb632cdac02159e1f
MarianoGH01 <40776619+MarianoGH01@users.noreply.github.com>
2018-07-05 21:58:25 -0300

    dia dos

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
 
+	var precio;
+	var descuento;
+	var descuentomoney
+	var preciocondesc;
+	var IVA;
+	var preciofinal;
+
+	precio = prompt("Ingrese el precio");
+	descuento = prompt("Ingrese el porcentaje de descuento");
+
+	descuentomoney = precio - (precio * descuento / 100);
+	preciocondesc = precio - descuentomoney;
+	IVA = precio * 0.21;
+
+	alert ('El descuento es de ' + preciocondesc + ' el precio con descuneto es de ' + descuentomoney + ' y el IVA es ' + IVA);
+
+	preciofinal = descuentomoney + IVA;
+	document.getElementById('elPrecioFinal').value = preciofinal;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	base = prompt("Ingrese la base de un  triangulo equilátero.");
+	altura = prompt("Ingrese la altura de un  triangulo equilátero.");
 
+	var superficie = ((base * altura) / 2);
+
+	var perimetro = 3 * base;
+
+	alert('La superficie del triangulo es ' + superficie + ' y su perimetro es ' + perimetro);
 }
