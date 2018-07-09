df58e6cfb63a254b716f53b87aae3f991263e259
MarianoGH01 <40776619+MarianoGH01@users.noreply.github.com>
2018-07-09 20:08:22 -0300

    housework 09072018

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,47 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño;
+mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
-
-
+switch(mesDelAño)
+{
+	case "Enero":
+		alert('El mes tiene 31 días.');
+		break;
+	case "Febrero":
+		alert('El mes tiene 28 días.');
+		break;
+	case "Marzo":
+		alert('El mes tiene 31 días.');
+		break;
+	case "Abril":
+		alert('El mes tiene 30 días.');
+		break;
+	case "Mayo":
+		alert('El mes tiene 31 días.');
+		break;
+	case "Junio":
+		alert('El mes tiene 30 días.');
+		break;					
+	case "Julio":
+		alert('El mes tiene 31 días');
+		break;
+	case "Agosto":
+		alert('El mes tiene 31 días');
+		break;		
+	case "Septiembre":
+		alert('El mes tiene 30 días.');		
+		break;
+	case "Octubre":
+		alert('El mes tiene 31 días');		
+		break;
+	case "Noviembre":
+		alert('El mes tiene 30 días');		
+		break;
+	case "Diciembre":
+		alert('El mes tiene 31 días');		
+		break;	
+};
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,28 @@
 function mostrar()
 {
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
+//tomo la hora  
+var laHora;
+laHora = parseInt(document.getElementById('hora').value);
 
-//alert (laHora);
-	
-	
-
-
+switch(laHora)
+{
+	case 7:
+		alert('Es de mañana.');
+		break; 
+	case 8:
+		alert('Es de mañana.');
+		break; 
+	case 9:
+		alert('Es de mañana.');
+		break; 
+	case 10:
+		alert('Es de mañana.');
+		break; 
+	case 11:
+		alert('Es de mañana.');
+		break; 
+	default:
+		break; 										
+};
 
 }//FIN DE LA FUNCIÓN
