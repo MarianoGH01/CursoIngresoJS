6aa80678990e9b07abcef2ad36dcd77902ffa4de
MarianoGH01 <40776619+MarianoGH01@users.noreply.github.com>
2018-07-09 17:46:49 -0300

    housework 09072018

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,11 +1,19 @@
 function mostrar()
 {
-//tomo la edad  
+
 var edad;
 var estadocivil;
-
+ 
+//tomo la edad   
 edad = document.getElementById('edad').value;
-estadocivil = document.getElementById('').value;	
+//tomo el estado civil
+estadocivil = document.getElementById('estadoCivil').value;	
+
+if(edad < 18 && estadocivil == 'Soltero')	
+{
+	//mensaje de alerta
+	alert('Es muy pequeño para NO ser soltero.');	
+};
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,17 @@
 function mostrar()
 {
-//tomo la edad  
 
+var edad;
+var estadocivil;
 	
+//tomo la edad  
+edad = document.getElementById('edad').value;
+//tomo el estado civil
+estadocivil = document.getElementById('estadoCivil').value;
 
+if(edad >= 18 && estadocivil == 'Soltero')
+{
+	alert('Es soltero y no es menor.');
+};
 
 }//FIN DE LA FUNCIÓN
