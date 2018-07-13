42af63b5f04305190c3766234fa30fbd6f421d0d
MarianoGH01 <40776619+MarianoGH01@users.noreply.github.com>
2018-07-12 22:14:22 -0300

    dia tres

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var laclave = "utn750";
+var Success = false;	
+
+while (!Success){
+	var clave = prompt("ingrese el número clave.");
+	Success = (laclave == clave);
+
+};
+
 
 
 }//FIN DE LA FUNCIÓN
