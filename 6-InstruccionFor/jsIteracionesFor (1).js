7bdb56a2daf5042d1a62f40d54c0db6e397c7d2a
MarianoGH01 <40776619+MarianoGH01@users.noreply.github.com>
2018-08-02 21:48:52 -0300

    dia ocho

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -3,10 +3,33 @@ al presionar el botón mostrar 10 repeticiones con números ASCENDENTE, desde el
 */
 function mostrar()
 {
+	
 	var tope;
 	tope = 10;
 
 	for(i = 1; i <= tope; i++){
 		alert('Nº ' + i);
 	};
+	
+	/*
+	var contador;
+	contador = 0;
+	for (;; ) {
+		console.log(contador);
+		contador++;
+		
+		if (contador == 5) {
+			break;	
+		};
+		
+	}
+	console.log('Final For ' + contador);
+	
+	contador = 0;
+	while(contador < 5){
+		console.log(contador);
+		contador++;
+	}
+	console.log('Final While ' + contador);
+	*/
 }
